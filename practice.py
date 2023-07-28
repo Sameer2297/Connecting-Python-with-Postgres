9c43e295b67d54afc010014a03b87861f284b728
Sameer2297 <123373057+Sameer2297@users.noreply.github.com>
2023-07-28 14:39:54 +0530

    Python to SQL(Postgres) connection

--- practice.py
+++ practice.py
@@ -2,7 +2,7 @@ import psycopg2
 
 try:
     con = psycopg2.connect(
-        user="sameer",
+        user="sameer",     # username of the host server
         password="sameer",
         host="localhost",    # Use "localhost" if the database is on your local machine
         port=5432,    # Usually 5432 is the default port for PostgreSQL
@@ -23,7 +23,7 @@ try:
     for row in rows:
         print(row)
 
-    #con.commit()
+    #con.commit()      # commit has to be used to make permanent changes to database. While using DML queries, we have to commit the changes in the connection, otherwise the changes won't effect database.
 
     #print("Data inserted succesfully!!!")
 
